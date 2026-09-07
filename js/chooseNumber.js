6042a14b0b768d4ddeadeef2d04b925e29f1da80
Grebennicova <ecaterina.grebennicova@stefanini.com>
2026-09-07 15:14:11 +0300

    feat: add history at adding values

--- js/chooseNumber.js
+++ js/chooseNumber.js
@@ -1,13 +1,18 @@
 import { selectedCell } from "./chooseCell.js";
+import { addToHistory } from "./history.js";
 
 export let selectedButton = null;
 
 export function chooseNumber(button) {
     selectedButton = button.id; 
-    
+
     if (selectedCell.classList.contains("fixed")) {
         return; 
     } else {
+        const oldValue = selectedCell.innerText;
         selectedCell.innerText = selectedButton;
+        const newValue = selectedCell.innerText;
+        
+        addToHistory(selectedCell.id, oldValue, newValue)
     }
 }
---
+++ js/history.js
@@ -0,0 +1,10 @@
+export let history = []
+
+export function addToHistory(id, oldValue, newValue) {
+    history.push({
+        cell: id,
+        oldValue: oldValue,
+        newValue: newValue
+    })
+    console.log(history)
+}
